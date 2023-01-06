e0d9642d8241789ff274acba5631557d88cc7c2c
tatayoyoh <tatayoyoh@mymail>
2023-01-06 10:25:00 +0100

    final version exo1

--- Exo1/exo1.py
+++ Exo1/exo1.py
@@ -6,7 +6,7 @@ class Robot():
     __power = False
     __current_speed = 0
     __battery_level = 0
-    __states = ['shutown', 'running']
+    __states = ['shutdown', 'running']
       
     """
       Give your best code here ( •̀ ω •́ )✧
@@ -26,6 +26,7 @@ class Robot():
         print("%s Auto destruction NOW"%(self.__name))
         global ROBOT_COUNT
         ROBOT_COUNT -= 1
+
     @property
     def name(self):
         return self.__name
@@ -43,47 +44,62 @@ class Robot():
         return self.__current_status
 
     def boot(self):
-    # Do someting to boot
         self.power = True
-        current_speed = 0
-        self.states = 'running'
+        self.__current_status = self.__states[1]
+        self.__current_speed = 0
 
     def shutdown(self):   
-        self.__power = False
+        self.power = False
         self.__current_status = self.__states[0]
-        self.__current_speed = 0
+        self.__current_speed = 0   
 
     def charge(self):
         print('Charging battery...')
-        
+        while self.__battery_level < 100:
+            self.__battery_level += 20
+            print("Charge is %s%%"%(self.battery_level))
+            time.sleep(1)
 
     def status(self):
-        print("%s status : %s [%s%% battery]"%(self.name, self.__current_status, self.battery_level)) 
+        print("%s Status: %s [%s%% Battery]"%(self.name, self.current_status, self.battery_level)) 
     
     def stop(self):
         self.__current_speed=0
 
     def move(self,speed):
-        if type(speed)==int:
-            self.__current_speed = speed
+        if self.current_status=='running' :
+            if self.battery_level>0:
+                if type(speed)==int:
+                    self.__current_speed = speed
+                else:
+                    print("Speed isn't a int ")
+            else:
+                print("Please charge your robot ")
+        else:
+            print("You need to boot your robot ")
 
     def speed(self):
         return self.__current_speed
-
+    
     def __str__(self): # must return a string
-        return "Robot %s [state: %s current speed : %s battery_level : %s ]"%(self.name, self.states, self.speed, self.__battery_level)
-  
-    #def __repr__(self): # must return a string
-     #   return str({"name":self.name, "size":self.size})
-
+        return "Robot %s [State: %s Current Speed : %s km/h Battery Level : %s ]"%(self.name, self.__current_status, self.__current_speed, self.__battery_level)
+    
 if __name__ == '__main__':
   r = Robot()
-  r.name = "Termonator"
+  r.name = "Wally"
+  r.move(100)
+  r.boot()
   r.move(100)
   r.charge()
-  print(r.status)
-  print(r.name + "\nSpeed:", r.speed())
-  r = Robot(name="Robotnik")
+  r.move(100)
+  print(r.name + "'s speed:", r.speed())
+  r.stop()
+  print(r.name + "'s speed:", r.speed())
+  r.shutdown()
+  r.boot()
+
+
+  r = Robot(name="Terminator")
   r.status()
   r.boot()
   r.status()
@@ -91,11 +107,7 @@ if __name__ == '__main__':
   r.shutdown()
   r.status()
   print(r)
-  print("\nEncore encore !")
-  r.status()
-  r.boot()
-  r.status()
-  r.charge()
-  r.shutdown()
-  r.status()
+  
+
+  
 
