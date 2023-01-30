2b8c2f52f121feb53ec6fb4cdb1eb5a3c71bdc33
ClaireGyt <121880717+ClaireGyt@users.noreply.github.com>
2023-01-30 09:38:42 +0100

    Delete simu-2023/controllers directory

--- simu-2023/controllers/my_controller/turtle_controller.py
+++
@@ -1,9 +0,0 @@
-from turtleRobot import TurtleRobot
-
-if __name__ == '__main__':
-    TIME_STEP = 64
-
-    # create the Robot instance.
-    robot = TurtleRobot(TIME_STEP)
-    robot.track_object('rubber duck')
-    robot.start_mission()
