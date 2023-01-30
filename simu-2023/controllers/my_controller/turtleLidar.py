bb4a4167b87b42e8b65d570c32ffe6eca831c359
ClaireGyt <121880717+ClaireGyt@users.noreply.github.com>
2023-01-30 08:47:12 +0100

    update

--- simu-2023/controllers/my_controller/turtleLidar.py
+++ simu-2023/controllers/my_controller/turtleLidar.py
@@ -1,13 +1,20 @@
-from controller import Lidar
+#from controller import Lidar
 
-class TurtleLidar(Lidar):
-    def __init__(self):
-        super().__init__("LDS-01")
-        self.enable(timestep)
+#class TurtleLidar(Lidar):
+  #  def __init__(self):
+      #  super().__init__("LDS-01")
+      #  self.enable(timestep)
 
 
         
+from controller import Lidar
 
+class Lidar_Controller():
+    def __init__(self, robot) -> None:
+        self. timestep = int(robot.getBasicTimeStep())
+        self.front_lidar =robot.getDevice('LDS-01')
+        self.front_lidar.enable(self.timestep)
+        self.front_lidar.enablePointCloud()
 
 
         
